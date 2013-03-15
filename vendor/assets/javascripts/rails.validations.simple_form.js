b63a9c496e9ebf09db75933e287633e8482872c6
Pairing Machine <pairing@edgecase.com>
2013-03-15 10:50:23 -0400

    Added support for Zurb Foundation

--- vendor/assets/javascripts/rails.validations.simple_form.js
+++ vendor/assets/javascripts/rails.validations.simple_form.js
@@ -1,9 +1,9 @@
 
 /*
-  Client Side Validations - SimpleForm - v2.0.0.rc.2
+  Client Side Validations - SimpleForm - v2.0.0
   https://github.com/dockyard/client_side_validations-simple_form
 
-  Copyright (c) 2012 DockYard, LLC
+  Copyright (c) 2013 DockYard, LLC
   Licensed under the MIT license
   http://www.opensource.org/licenses/mit-license.php
 */
@@ -45,18 +45,18 @@
       bootstrap: {
         add: function(element, settings, message) {
           var errorElement, wrapper_class_element, wrapper_tag_element;
-          if (element.data('valid') !== false) {
-            wrapper_class_element = element.closest("." + settings.wrapper_class);
+          errorElement = element.parent().find("" + settings.error_tag + "." + settings.error_class);
+          if (errorElement[0] == null) {
             wrapper_tag_element = element.closest(settings.wrapper_tag);
-            wrapper_class_element.addClass(settings.wrapper_error_class);
             errorElement = $("<" + settings.error_tag + "/>", {
               "class": settings.error_class,
               text: message
             });
-            return wrapper_tag_element.append(errorElement);
-          } else {
-            return element.parent().find("" + settings.error_tag + "." + settings.error_class).text(message);
+            wrapper_tag_element.append(errorElement);
           }
+          wrapper_class_element = element.closest("." + settings.wrapper_class);
+          wrapper_class_element.addClass(settings.wrapper_error_class);
+          return errorElement.text(message);
         },
         remove: function(element, settings) {
           var errorElement, wrapper_class_element, wrapper_tag_element;
@@ -64,6 +64,31 @@
           wrapper_tag_element = element.closest(settings.wrapper_tag);
           wrapper_class_element.removeClass(settings.wrapper_error_class);
           errorElement = wrapper_tag_element.find("" + settings.error_tag + "." + settings.error_class);
+          return errorElement.remove();
+        }
+      },
+      foundation: {
+        add: function(element, settings, message) {
+          var errorElement, wrapper_class_element, wrapper_tag_element;
+          errorElement = element.parent().find("" + settings.error_tag + "." + settings.error_class);
+          if (errorElement[0] == null) {
+            wrapper_tag_element = element.closest(settings.wrapper_tag);
+            errorElement = $("<" + settings.error_tag + "/>", {
+              "class": settings.error_class,
+              text: message
+            });
+            wrapper_tag_element.append(errorElement);
+          }
+          wrapper_class_element = element.closest("." + settings.wrapper_class);
+          wrapper_class_element.addClass(settings.wrapper_error_class);
+          return errorElement.text(message);
+        },
+        remove: function(element, settings) {
+          var errorElement, wrapper_class_element, wrapper_tag_element;
+          wrapper_class_element = element.closest("." + settings.wrapper_class + "." + settings.wrapper_error_class);
+          wrapper_tag_element = element.closest(settings.wrapper_tag);
+          wrapper_class_element.removeClass(settings.wrapper_error_class);
+          errorElement = wrapper_tag_element.find("" + settings.error_tag);
           return errorElement.remove();
         }
       }
